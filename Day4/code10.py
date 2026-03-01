ef08025aac6df5ccf55818c0336f2b0b434695c6
Manini-Ojha <114860429+Manini-Ojha@users.noreply.github.com>
2026-03-01 20:43:21 +0530

    Create code10.py with examples of continue and pass
    Add code10.py demonstrating continue and pass keywords

---
+++ Day4/code10.py
@@ -0,0 +1,9 @@
+#Continue keyword
+
+#skipping an iteration at a specific condition
+
+#Pass keyword
+
+#if block requires statements and if we don't have a statement block then an error will occur so to allow an empty if block, we use pass, same for for loops, classes and functions.
+#it is used to make an abstract method
+#range (from the for loop syntax) is an iterable object
